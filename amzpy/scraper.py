435442adf5515f9d9c63a7130d63609e4a1f6e2c
Matthias Guth <ChiSamurai@users.noreply.github.com>
2026-02-17 16:11:55 +0100

    added proxy_request

--- amzpy/scraper.py
+++ amzpy/scraper.py
@@ -99,6 +99,14 @@ class AmazonScraper:
         
         return self.user_config
 
+    def proxy_request(self, url_string: str):
+        url = "https://www.amazon.{self.country_code}/{url_string}"
+        response = self.session.get(url)
+            if not response or not response.text:
+                print(f"Failed to fetch search page: {url}")
+                break
+        return response
+
     def get_product_details(self, url: str) -> Optional[Dict]:
         """
         Fetch and parse details for a product using its Amazon URL.
