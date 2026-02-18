6f2b666ec60953efd9f5d9213824c7bfb9b598da
Matthias Guth <ChiSamurai@users.noreply.github.com>
2026-02-18 18:46:08 +0100

    Change default Accept-Language to German

--- amzpy/session.py
+++ amzpy/session.py
@@ -30,7 +30,7 @@ DEFAULT_CONFIG = {
 # Default header template
 DEFAULT_HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Language': 'de-DE,de;q=0.9',
     'Upgrade-Insecure-Requests': '1',
     'Sec-Ch-Ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
     'Sec-Ch-Ua-Mobile': '?0',
